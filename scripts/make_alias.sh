fa1f39e213162a76efd97b3897297f8da1f27f2c
Luca Gorayeb <lucagorayeb@gmail.com>
2026-05-08 21:11:57 -0400

    updating

--- scripts/make_alias.sh
+++ scripts/make_alias.sh
@@ -10,8 +10,9 @@
 # Uso:
 # -----------------------------------------------------
 
+ls ~/documents/bash/scripts/
 read -p "What will be the name of the alias? " name_alias;
 read -p "What is the path of the alias? " path_alias
 echo "alias $name_alias='$path_alias'" >> ~/.bash_aliases
-sleep 1
+sleep 2 
 . ~/.bashrc
